5ebd6b545e86b42ed6f1c533eb648c032b48ba08
田　英毅 <denhdk1997@yahoo.co.jp>
2018-12-15 19:57:11 +0900

    mechanizeの追記とproductsテーブル作成

--- db/migrate/20181215074741_create_products.rb
+++ db/migrate/20181215074741_create_products.rb
@@ -1,7 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[5.2]
   def change
     create_table :products do |t|
-
+      t.string :title
+      t.string :director
+      t.text   :image_url
+      t.integer :page
       t.timestamps
     end
   end

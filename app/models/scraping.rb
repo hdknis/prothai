10232ad238733e2711a1b62bf93db65062c3732c
田　英毅 <denhdk1997@yahoo.co.jp>
2019-01-11 13:40:04 +0900

    タイモブスクレイピングテスト

--- app/models/scraping.rb
+++ app/models/scraping.rb
@@ -3,14 +3,14 @@ class Scraping
     agent = Mechanize.new
     links = []
 
-      current_page = agent.get("https://bookmeter.com/rankings")
-      elements = current_page.search('.detail__title a')
+      current_page = agent.get("https://www.tigermov.com/internship/")
+      elements = current_page.search('.internBox__article a')
       elements.each do |ele|
         links << ele.get_attribute('href')
       end
 
     links.each do |link|
-       get_product('https://bookmeter.com' + link)
+       get_product('https://www.tigermov.com' + link)
     end
 
   end
@@ -36,15 +36,41 @@ class Scraping
     puts link
     agent = Mechanize.new
     page = agent.get(link)
-    title = page.at('.inner__title').inner_text if page.at('.inner__title')
-    image_url = page.at('.group__image img')[:src] if page.at('.group__image img')
-    director = page.at('.header__authors a').inner_text if page.at('.header__authors a')
-    page = page.at('.bm-details-side__item span').inner_text if page.at('.bm-details-side__item span')
+    title = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    catch_copy = page.at('.detailIntro__txt').inner_text if page.at('.detailIntro__txt')
+    business_content = page.at('.detailMain__article__txt').inner_text if page.at('.detailMain__article__txt')
+    experience_content = page.at('.detailMain__article__txt p').inner_text if page.at('.detailMain__article__txt p')
+    request_content = page.at('.detailMain__article__txt p').inner_text if page.at('.detailMain__article__txt p')
+    application_requirement = page.at('.bm-details-side__item span').inner_text if page.at('.bm-details-side__item span')
+    country = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    city = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    period = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    days = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    time = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    allowance = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    company_introduction = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    company_introduction_picture = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    employees_number = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    language = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
+    manager_profile = page.at('.sectionBox__h1__type2').inner_text if page.at('.sectionBox__h1__type2')
 
     product = Product.where(title: title).first_or_initialize
-    product.image_url = image_url
-    product.director = director
-    product.page = page
+    product.catch_copy = catch_copy
+    product.business_content = business_content
+    product.experience_content = experience_content
+    product.request_content = request_content
+    product.application_requirement = application_requirement
+    product.country = country
+    product.city = city
+    product.period = period
+    product.days = days
+    product.time = time
+    product.allowance = allowance
+    product.company_introduction = company_introduction
+    product.company_introduction_picture = company_introduction_picture
+    product.employees_number = employees_number
+    product.language = language
+    product.manager_profile = manager_profile
     product.save
   end
 end

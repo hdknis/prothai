a627d3c8139ab33a1ad5000048705abd0dfcf3e1
田　英毅 <denhdk1997@yahoo.co.jp>
2019-01-22 16:46:21 +0900

    関連企業の雛形作成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,6 +10,7 @@ class ProductsController < ApplicationController
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
     @product = Product.find(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
+    @relation_products = Product.order('id ASC').limit(5)
   end
 
   def search

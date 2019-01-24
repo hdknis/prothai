2255bfbfbbaa15394f6d7de270113380a28d957e
田　英毅 <denhdk1997@yahoo.co.jp>
2019-01-24 15:59:22 +0900

    bootstrap導入初期設定

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
-//= require jquery
 //= require jquery_ujs
 //= require jquery-ui
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets

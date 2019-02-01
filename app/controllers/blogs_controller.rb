7792fd5a8b72ffd871b76301e3643569348d3db2
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-02 00:25:05 +0900

    blogモデルshowビューの削除とindexビューの文言変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,9 +4,4 @@ class BlogsController < ApplicationController
     @blogs = Blog.order('id ASC').limit(30)
   end
 
-  def show
-    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @blog = Blog.find(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
-  end
-
 end

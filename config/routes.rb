ca13ba6164d668f1e3362041b198eff9ba58ff3b
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-06 00:05:41 +0900

    rails_adminのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'products#index'
   resources :products, only: :show
   get 'static_pages/about'

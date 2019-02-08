fe4aab3044879cbe105b03f3bfe762f5b8d76c69
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-08 12:36:26 +0900

    サイドバーへのモーダル追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,19 @@
 class ProductsController < ApplicationController
-  def index
-    # productsテーブルから最新順に作品を２０件取得する
+  before_action :create_info , only: [:index, :show]
+
+  def create_info
     @product = Product.new
-    @products = Product.order('id ASC').limit(20)
     @periods = @product.create_period
     @countrys = @product.create_country
   end
 
-  def show
-    # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @product = Product.find(params[:id]) # 問題3ではこのコードは消して新しくコードを書いてください
+  def index
+    @products = Product.order('id ASC').limit(20)
+  end
 
-    # @products = Product.find()
+  def show
+    @product = Product.find(params[:id])
     @relation_products = Product.order('id ASC').limit(5)
-    @periods = @product.create_period
     @connect_tags = @product.connect_tags
   end
 end

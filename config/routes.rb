04d26ef3c8f89be4455e1f4c26637d5feb243ee3
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-09 15:23:31 +0900

    お問い合わせページのデザイン修正とバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  root 'products#index'
-  resources :products, only: :show
+  root 'static_pages#home'
+  resources :products, only: [:index,:show]
   get 'static_pages/about'
   get 'static_pages/member'
   get 'static_pages/member_recruit'
   resources :blogs, only: :index
   get 'inquirys' => 'inquirys#index'              # 入力画面
+  get 'inquirys/confirm' => redirect("/inquirys")
+  get 'inquirys/thanks' => redirect("/inquirys")
   post 'inquirys/confirm' => 'inquirys#confirm'   # 確認画面
   post 'inquirys/thanks' => 'inquirys#thanks'     # 送信完了画面
 end

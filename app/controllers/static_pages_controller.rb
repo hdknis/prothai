a0db7ea7356f6f0fafb68d3ec473e37958cd9a35
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-14 21:45:53 +0900

    文言追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,5 +11,22 @@ class StaticPagesController < ApplicationController
 
 	def member_recruit
 	  @relation_products = Product.order('id ASC').limit(5)
+	  @Recruitment_content = "①インタビュー記事ライター
+
+海外で働く経営者の声を記事にまとめて、発信していただきます!
+
+②ビジネスサイドメンバー、WEBディレクター
+
+『MashUp』の運営方針を一緒に考え、今後の組織運営を担っていただきます! また、マネタイズに向けた動きも一緒に考えていただきたいです。"
+      @application_method = '<a href="/inquirys">お問い合わせフォーム</a>から
+
+      お名前
+
+      Eメールアドレス
+
+      電話番号
+
+      の３点を記入の上、お問い合わせ内容に希望している職種の入力をお願いします。
+      追って、こちらから連絡をさせて頂きます。'
 	end
 end

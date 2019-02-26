745ac32b35868dae6df16e419d88f103fe4c88b3
田　英毅 <denhdk1997@yahoo.co.jp>
2019-02-26 20:13:16 +0900

    国DB追加情報

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_14_041016) do
+ActiveRecord::Schema.define(version: 2019_02_15_095844) do
 
   create_table "blogs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "user_name"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 2019_02_14_041016) do
     t.datetime "updated_at", null: false
     t.index ["product_id"], name: "index_connect_tags_on_product_id"
     t.index ["tag_id"], name: "index_connect_tags_on_tag_id"
+  end
+
+  create_table "countries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "cost_information"
+    t.text "flag_pic"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "feedbacks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|

532f7022665925a71698f7afe74fb647e035168a
田　英毅 <denhdk1997@yahoo.co.jp>
2019-03-19 17:27:08 +0900

    リダイレクトテスト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   FQDN = 'mash-up1221.com'
   # redirect correct server from herokuapp domain for SEO
   def ensure_domain
-   return unless /\.herokuapp.com/ =~ request.host || /\.www.mash-up1221.com.com/ =~ request.host
+   return unless /\.herokuapp.com/ =~ request.host || /www.mash-up1221.com/ =~ request.host
    # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
    port = ":#{request.port}" unless [80, 443].include?(request.port)
    redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently

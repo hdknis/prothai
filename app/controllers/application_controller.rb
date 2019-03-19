2775f95d660ec7472ec6cda5af2a20ddc27d2aeb
田　英毅 <denhdk1997@yahoo.co.jp>
2019-03-19 16:56:20 +0900

    herokuのドメインに行くとルートドメインにリダイレクト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::Base
 	layout 'review_site'
+	protect_from_forgery with: :exception
 	def after_sign_out_path_for(resource)
      rails_admin_path
     end
+
+  #herokuapp.comから独自ドメインへリダイレクト
+  before_filter :ensure_domain
+  FQDN = 'mash-up1221.com'
+  # redirect correct server from herokuapp domain for SEO
+  def ensure_domain
+   return unless /\.herokuapp.com/ =~ request.host
+   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+   port = ":#{request.port}" unless [80, 443].include?(request.port)
+   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  end
 end

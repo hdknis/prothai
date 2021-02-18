8e0a2b7a401e66a7a561d173e28869a0d085fd96
田　英毅 <denhdk1997@yahoo.co.jp>
2021-02-18 17:28:35 +0900

    herokuURLからのリダイレクトアクションの停止

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,17 @@
 class ApplicationController < ActionController::Base
-	before_action :ensure_domain
+	# before_action :ensure_domain
 	protect_from_forgery with: :exception
 	def after_sign_out_path_for(resource)
      rails_admin_path
   end
 
   #herokuapp.comから独自ドメインへリダイレクト
-  FQDN = 'mash-up1221.com'
-  # redirect correct server from herokuapp domain for SEO
-  def ensure_domain
-   return unless /\.herokuapp.com/ =~ request.host || /www.mash-up1221.com/ =~ request.host
-   # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
-   port = ":#{request.port}" unless [80, 443].include?(request.port)
-   redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
-  end
+  # FQDN = 'mash-up1221.com'
+  # # redirect correct server from herokuapp domain for SEO
+  # def ensure_domain
+  #  return unless /\.herokuapp.com/ =~ request.host || /www.mash-up1221.com/ =~ request.host
+  #  # 主にlocalテスト用の対策80と443以外でアクセスされた場合ポート番号をURLに含める
+  #  port = ":#{request.port}" unless [80, 443].include?(request.port)
+  #  redirect_to "#{request.protocol}#{FQDN}#{port}#{request.path}", status: :moved_permanently
+  # end
 end
